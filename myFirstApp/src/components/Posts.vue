22b61352b65c10de979866aec34951a87e2861bd
Muhammad Saqib <66180927+MuhammadSaqib786@users.noreply.github.com>
2022-09-18 13:09:07 +0500

    added body in posts

--- myFirstApp/src/components/Posts.vue
+++ myFirstApp/src/components/Posts.vue
@@ -9,13 +9,14 @@
     <tr>
       <th scope="col">Id</th>
       <th scope="col">Language</th>
+      <th scope="col">Usage</th>
     </tr>
   </thead>
   <tbody>
-    <tr v-for='post in posts' @key="id">
+    <tr v-for='post in posts' @key="post.id">
       <th scope="row">{{post.id}}</th>
       <td>{{post.title}}</td>
-      
+      <td>{{post.body}}</td>
     </tr>
     
   </tbody>
@@ -32,12 +33,12 @@ export default{
     data(){
         return{
             posts:[
-                {id:1,title:'C++'},
-                {id:2,title: 'Java'},
-                {id:3,title:'Python'},
-                {id:4,title: 'C#'},
-                {id:5,title:'VB'},
-                {id:6,title: 'Kotlin'},
+                {id:1,title:'C++',body:'This is simple langauge to start learn programming.'},
+                {id:2,title: 'Java',body:'This is more advanced langauge.'},
+                {id:3,title:'Python',body:'Best langauge used for machine learning and AI.'},
+                {id:4,title: 'C#',body:'Best for web app development purposes'},
+                {id:5,title:'VB',body:'Thats also alternate to C# langauge.'},
+                {id:6,title: 'Kotlin',body:'Best langauge used now a days for android application development.'},
             ]
         }
     }
