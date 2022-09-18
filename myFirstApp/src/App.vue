bbff0b76b91cc4a3cb83165da87b88546a412011
Muhammad Saqib <66180927+MuhammadSaqib786@users.noreply.github.com>
2022-09-18 12:56:36 +0500

    using data in component using loop

--- myFirstApp/src/App.vue
+++ myFirstApp/src/App.vue
@@ -23,12 +23,5 @@ export default {
 <style>
 
   @import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- myFirstApp/src/components/Posts.vue
+++ myFirstApp/src/components/Posts.vue
@@ -1,8 +1,12 @@
 <template>
 <div>
-    <h1>
+    <h3>
         Hi.. Welcome Saqib to your first component
-    </h1>
+    </h3>
+    Below are some langauges you should learn:
+    <ul>
+        <li v-for='post in posts' @key="id">{{post.title}}</li>
+    </ul>
     <button class="btn btn-primary">Let's Get Started</button>
 </div>
 </template>
@@ -13,7 +17,14 @@ export default{
     name: 'PostExample',
     data(){
         return{
-
+            posts:[
+                {id:1,title:'C++'},
+                {id:2,title: 'Java'},
+                {id:3,title:'Python'},
+                {id:4,title: 'C#'},
+                {id:5,title:'VB'},
+                {id:6,title: 'Kotlin'},
+            ]
         }
     }
 }
